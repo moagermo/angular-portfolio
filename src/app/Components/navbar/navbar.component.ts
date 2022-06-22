b068b33dc97bfc4c38c2799e92a826ea6de58c75
Mo Ager <moagermo@gmail.com>
2022-06-21 20:02:52 -0400

    Routing changes

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { preserveWhitespacesDefault } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,23 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  aboutMePage = document.getElementById('#aboutMePage');
-  projectsPage = document.getElementById('#projectsPage');
-  educationPage = document.getElementById('#educationPage');
-  experiencePage = document.getElementById('#experiencePage');
 
   iconSrc = "../../assets/images/binary-code.svg"
-  constructor() { }
+  classAboutMe = 'btn btn-outline-info active';
+  classProject = 'btn btn-outline-info';
+  classExperience = 'btn btn-outline-info';
+  classEducation = 'btn btn-outline-info active';
+
+  public href: string = '';
+  url: string;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-  
-  clickOfLink(this: any){
-    this.aboutMePage.className.remove(' active');
-    this.projectsPage.className.remove(' active');
-    this.experiencePage.className.remove(' active');
-    this.educationPage.className.remove(' active');
-    this.className+=' active'
 
+  setActiveButton(button: string){
+    if(button === 'aboutMePage'){
+      this.classAboutMe = 'btn btn-outline-info active';
+      this.classProject = 'btn btn-outline-info';
+      this.classEducation = 'btn btn-outline-info';
+      this.classExperience = 'btn btn-outline-info';
+    } else if (button === 'projectsPage'){
+      this.classAboutMe = 'btn btn-outline-info';
+      this.classProject = 'btn btn-outline-info active';
+      this.classEducation = 'btn btn-outline-info';
+      this.classExperience = 'btn btn-outline-info';
+    } else if (button === 'experiencePage'){
+      this.classAboutMe = 'btn btn-outline-info';
+      this.classProject = 'btn btn-outline-info';
+      this.classEducation = 'btn btn-outline-info';
+      this.classExperience = 'btn btn-outline-info active';
+    } else if (button === 'educationPage'){
+      this.classAboutMe = 'btn btn-outline-info';
+      this.classProject = 'btn btn-outline-info';
+      this.classEducation = 'btn btn-outline-info active';
+      this.classExperience = 'btn btn-outline-info';
+    }
   }
 }
--- src/app/Pages/about-me/about-me.component.ts
+++ src/app/Pages/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from 'src/app/Components/navbar/navbar.component';
 
 @Component({
     selector: 'about-me-page',
@@ -8,8 +9,8 @@ import { Component, OnInit } from '@angular/core';
 
 export class AboutMeComponent implements OnInit {
     profilePicture = '../../../assets/images/profile-pic.jpg'
+
     constructor() {}
     ngOnInit(): void {
-        
     }
 }
